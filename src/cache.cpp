6d40cf9611c4756dccfb32e0b4ae13d7ca8f39bd
Ori Dabush <dabushori@gmail.com>
2020-08-30 00:47:52 +0300

    formatted the code

--- src/cache.cpp
+++ src/cache.cpp
@@ -2,36 +2,35 @@
 
 namespace cache {
 
-Cache::Cache(const std::string& fileName) {
-    m_fileName = fileName;
-}
+Cache::Cache(const std::string &fileName) { m_fileName = fileName; }
 
-MatCache::MatCache(const std::string& fileName) : Cache(fileName) {};
+MatCache::MatCache(const std::string &fileName) : Cache(fileName){};
 
-void MatCache::save(const std::vector<std::string>& lines) const {
-    std::string path = ""; // need the path
-    path.append(m_fileName);
-    std::ofstream out(path, std::ios::trunc);
-    if (!out) {
-        //error
-    }
-    for (auto it = lines.begin(); it != lines.end(); ++it) {
-        out << *it << std::endl;
-    }
+void MatCache::save(const std::vector<std::string> &lines) const {
+  std::string path = ""; // need the path
+  path.append(m_fileName);
+  std::ofstream out(path, std::ios::trunc);
+  if (!out) {
+    // error
+  }
+  for (auto it = lines.begin(); it != lines.end(); ++it) {
+    out << *it << std::endl;
+  }
 }
 
 void MatCache::clear() const {
-    std::string path = ""; // need the path
-    path.append(m_fileName);
-    if(remove(path.c_str()) != 0) {
-    //error
-    }
+  std::string path = ""; // need the path
+  path.append(m_fileName);
+  if (remove(path.c_str()) != 0) {
+    // error
+  }
 }
 
-std::vector<std::string> MatCache::search(const std::vector<std::string>& lines) const {
-    std::string path = ""; // need the path
-    path.append(m_fileName);
-    std::ofstream out(path, std::ios::trunc);
+std::vector<std::string>
+MatCache::search(const std::vector<std::string> &lines) const {
+  std::string path = ""; // need the path
+  path.append(m_fileName);
+  std::ofstream out(path, std::ios::trunc);
 }
 
-}
+} // namespace cache
--- src/cache.h
+++ src/cache.h
@@ -1,31 +1,33 @@
 #pragma once
 
-#include <vector>
-#include <string>
 #include <fstream>
 #include <iostream>
-#include <ostream>
 #include <iterator>
+#include <ostream>
 #include <string.h>
+#include <string>
+#include <vector>
 
 namespace cache {
 
 class Cache {
 protected:
-    std::string m_fileName;
+  std::string m_fileName;
+
 public:
-    Cache(const std::string& fileName);
-    virtual void save(const std::vector<std::string>& lines) const=0;
-    virtual std::vector<std::string> search(const std::vector<std::string>& lines) const=0;
-    virtual void clear() const=0;
+  Cache(const std::string &fileName);
+  virtual void save(const std::vector<std::string> &lines) const = 0;
+  virtual std::vector<std::string>
+  search(const std::vector<std::string> &lines) const = 0;
+  virtual void clear() const = 0;
 };
 
 class MatCache : public Cache {
-    public:
-    MatCache(const std::string& fileName);
-    virtual void save(const std::vector<std::string>& lines) const;
-    virtual std::vector<std::string> search(const std::vector<std::string>& lines) const;
-    virtual void clear() const;
-
+public:
+  MatCache(const std::string &fileName);
+  virtual void save(const std::vector<std::string> &lines) const;
+  virtual std::vector<std::string>
+  search(const std::vector<std::string> &lines) const;
+  virtual void clear() const;
 };
 } // namespace cache
