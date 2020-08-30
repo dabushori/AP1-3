ff1e37a4d4087958d0789bc61e680422b9d2d1dd
Ori Dabush <dabushori@gmail.com>
2020-08-31 02:26:15 +0300

    made some chages in the functions and signatures

--- src/cache_manager.cpp
+++ src/cache_manager.cpp
@@ -1,23 +1,12 @@
 #include "cache_manager.h"
 
-#include "bmp_tester.hpp"
-#include "cache.h"
+#include "Cache.h"
 #include "mat_parser.h"
 
-#include <fstream>
-#include <iostream>
+#include <string>
 #include <vector>
 
 namespace cache_manager {
-bool isValidFile(const std::string &path) {
-  std::ifstream in(path);
-  if (!in) {
-    return false;
-  }
-  in.close();
-  return true;
-}
-
 std::vector<std::string> readFileLines(const std::string &filename) {
   std::ifstream in(filename);
   std::vector<std::string> lines;
@@ -28,199 +17,32 @@ std::vector<std::string> readFileLines(const std::string &filename) {
   return lines;
 }
 
-void addMatrices(const std::string &inputfile1, const std::string &inputfile2,
-                 const std::string &outputfile) {
-  if (!isValidFile(inputfile1) || !isValidFile(inputfile2)) {
-    // error
-  } else {
-    std::vector<std::string> input1 = readFileLines(inputfile1);
-    std::vector<std::string> input2 = readFileLines(inputfile2);
-    for (auto it = input1.begin(); it != input1.end(); ++it) {
-      mat_parser::deleteSpaces(*it);
-    }
-    for (auto it = input2.begin(); it != input2.end(); ++it) {
-      mat_parser::deleteSpaces(*it);
-    }
-    std::vector<std::string> input;
-    input.push_back("add");
-    input.push_back("lmatrix:");
-    for (auto it = input1.begin(); it != input1.end(); ++it) {
-      input.push_back(*it);
-    }
-    input.push_back("rmatrix:");
-    for (auto it = input2.begin(); it != input2.end(); ++it) {
-      input.push_back(*it);
-    }
+void addMatrices(const std::string &lmatrix, const std::string &rmatrix,
+                 const std::string &output) {
+  cache::Cache cache(MATCACHE_PATH, 2);
 
-    cache::MatCache cache("OurMatCache.txt"); // need file name
-    auto result = cache.search(input);
-    if (result == std::vector<std::string>()) {
-      matrix::Mat res;
-      matrix::Mat lmatrix = mat_parser::textToMat(input1);
-      matrix::Mat rmatrix = mat_parser::textToMat(input2);
-      try {
-        res = lmatrix.add(rmatrix);
-        result = mat_parser::matToText(res);
+  std::vector<std::string> inputs;
+  inputs.push_back(lmatrix);
+  inputs.push_back(rmatrix);
 
-        auto solution = input;
-        for (auto it = result.begin(); it != result.end(); ++it) {
-          solution.push_back(*it);
-        }
-
-        cache.save(solution);
-
-      } catch (const std::exception &e) {
-        // error
-      }
-    }
-
-    if (outputfile == "stdout") {
-      for (auto it = result.begin(); it != result.end(); ++it) {
-        std::cout << *it << std::endl;
-      }
-    } else {
-      std::ofstream out(outputfile, std::ios::trunc);
-      if (!out) {
-        // error
-      }
-      for (auto it = result.begin(); it != result.end(); ++it) {
-        out << *it << std::endl;
-      }
-      if (!out) {
-        // error
-      }
-    }
-  }
+  auto result = cache.search(inputs);
 }
+void multMatrices(const std::string &lmatrix, const std::string &rmatrix,
+                  const std::string &output);
 
-void multiplyMatrices(const std::string &inputfile1,
-                      const std::string &inputfile2,
-                      const std::string &outputfile) {
-  if (!isValidFile(inputfile1) || !isValidFile(inputfile2)) {
-    // error
-  } else {
-    std::vector<std::string> input1 = readFileLines(inputfile1);
-    std::vector<std::string> input2 = readFileLines(inputfile2);
-    for (auto it = input1.begin(); it != input1.end(); ++it) {
-      mat_parser::deleteSpaces(*it);
-    }
-    for (auto it = input2.begin(); it != input2.end(); ++it) {
-      mat_parser::deleteSpaces(*it);
-    }
-    std::vector<std::string> input;
-    input.push_back("mult");
-    input.push_back("lmatrix:");
-    for (auto it = input1.begin(); it != input1.end(); ++it) {
-      input.push_back(*it);
-    }
-    input.push_back("rmatrix:");
-    for (auto it = input2.begin(); it != input2.end(); ++it) {
-      input.push_back(*it);
-    }
+void rotateImage(const std::string &input, const std::string &output);
+void convertImageToGrayscale(const std::string &input,
+                             const std::string &output);
 
-    cache::MatCache cache("OurMatCache.txt"); // need file name
-    auto result = cache.search(input);
-    if (result == std::vector<std::string>()) {
-      matrix::Mat res;
-      matrix::Mat lmatrix = mat_parser::textToMat(input1);
-      matrix::Mat rmatrix = mat_parser::textToMat(input2);
-      try {
-        res = lmatrix.multiplyMatrices(rmatrix);
-        result = mat_parser::matToText(res);
-
-        auto solution = input;
-        for (auto it = result.begin(); it != result.end(); ++it) {
-          solution.push_back(*it);
-        }
-
-        cache.save(solution);
-
-      } catch (const std::exception &e) {
-        // error
-      }
-    }
-
-    if (outputfile == "stdout") {
-      for (auto it = result.begin(); it != result.end(); ++it) {
-        std::cout << *it << std::endl;
-      }
-    } else {
-      std::ofstream out(outputfile, std::ios::trunc);
-      if (!out) {
-        // error
-      }
-      for (auto it = result.begin(); it != result.end(); ++it) {
-        out << *it << std::endl;
-      }
-      if (!out) {
-        // error
-      }
-    }
-  }
-}
-
-void rotateImage(const std::string &inputfile, const std::string &outputfile) {
-  cache::ImageCache cache("OurImageCache.txt"); // need file name
-  std::vector<std::string> input;
-  input.push_back(inputfile);
-
-  auto result = cache.search(input);
-
-  if (result == std::vector<std::string>()) {
-    testing::bmp::rotate_image(inputfile, outputfile);
-  } else {
-    std::ofstream out(outputfile, std::ios::trunc);
-    std::ifstream in(result[0]);
-    if (!in || !out) {
-      // error
-    }
-    auto info = std::vector<char>{std::istreambuf_iterator<char>{in},
-                                  std::istreambuf_iterator<char>{}};
-    for (auto it = info.begin(); it != info.end(); ++it) {
-      out << *it;
-    }
-    if (!out) {
-      // error
-    }
-  }
-}
-
-void convertImage(const std::string &inputfile, const std::string &outputfile) {
-  cache::ImageCache cache("OurImageCache.txt"); // need file name
-  std::vector<std::string> input;
-  input.push_back(inputfile);
-
-  auto result = cache.search(input);
-
-  if (result == std::vector<std::string>()) {
-    testing::bmp::convert_to_grayscale(inputfile, outputfile);
-  } else {
-    std::ofstream out(outputfile, std::ios::trunc);
-    std::ifstream in(result[0]);
-    if (!in || !out) {
-      // error
-    }
-    auto info = std::vector<char>{std::istreambuf_iterator<char>{in},
-                                  std::istreambuf_iterator<char>{}};
-    for (auto it = info.begin(); it != info.end(); ++it) {
-      out << *it;
-    }
-    if (!out) {
-      // error
-    }
-  }
-}
-
-void hash(const std::string &algorithm, const std::string &inputfile,
-          const std::string &outputfile);
+void hash(const std::string &input, const std::string &output);
+uint32_t hash(const std::string &input);
 
 void clearCache();
-
-void searchInCache(const std::string &searchedFunction,
-                   const std::string &inputfile1, const std::string &inputfile2,
-                   const std::string &outputfile);
-
-void searchInCache(const std::string &searchedFunction,
-                   const std::string &inputfile, const std::string &outputfile);
-
+void searchInMatrixCache(const std::string &function,
+                         const std::string &lmatrix,
+                         const std::string &rmatrix);
+void searchInImageCache(const std::string &function, const std::string &lmatrix,
+                        const std::string &rmatrix);
+void searchInHashCache(const std::string &function, const std::string &lmatrix,
+                       const std::string &rmatrix);
 } // namespace cache_manager
--- src/cache_manager.h
+++ src/cache_manager.h
@@ -1,27 +1,26 @@
 #pragma once
 
 #include <string>
-#include <vector>
 
 namespace cache_manager {
-bool isValidFile(const std::string &path);
-std::vector<std::string> readFileLines(std::ifstream in);
+void addMatrices(const std::string &lmatrix, const std::string &rmatrix,
+                 const std::string &output);
+void multMatrices(const std::string &lmatrix, const std::string &rmatrix,
+                  const std::string &output);
 
-void addMatrices(const std::string &inputfile1, const std::string &inputfile2,
-                 const std::string &outputfile);
-void multiplyMatrices(const std::string &inputfile1,
-                      const std::string &inputfile2,
-                      const std::string &outputfile);
-void rotateImage(const std::string &inputfile, const std::string &outputfile);
-void convertImage(const std::string &inputfile, const std::string &outputfile);
-void hash(const std::string &algorithm, const std::string &inputfile,
-          const std::string &outputfile);
+void rotateImage(const std::string &input, const std::string &output);
+void convertImageToGrayscale(const std::string &input,
+                             const std::string &output);
+
+void hash(const std::string &input, const std::string &output);
+uint32_t hash(const std::string &input);
+
 void clearCache();
-void searchInCache(const std::string &cacheType,
-                   const std::string &searchedFunction,
-                   const std::string &inputfile1,
-                   const std::string &inputfile2);
-void searchInCache(const std::string &cacheType,
-                   const std::string &searchedFunction,
-                   const std::string &inputfile);
+void searchInMatrixCache(const std::string &function,
+                         const std::string &lmatrix,
+                         const std::string &rmatrix);
+void searchInImageCache(const std::string &function, const std::string &lmatrix,
+                        const std::string &rmatrix);
+void searchInHashCache(const std::string &function, const std::string &lmatrix,
+                       const std::string &rmatrix);
 } // namespace cache_manager
