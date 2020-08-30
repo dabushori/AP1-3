875628cb8fd8756cce54bbcf33bbcb97b9515a95
Ori Dabush <dabushori@gmail.com>
2020-08-31 01:15:27 +0300

    added a method signature

--- src/mat_parser.h
+++ src/mat_parser.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 namespace mat_parser {
+void deleteSpaces(std::string &str);
 std::vector<std::string> matToText(const matrix::Mat &matrix);
 matrix::Mat textToMat(const std::vector<std::string> &text);
 } // namespace mat_parser
