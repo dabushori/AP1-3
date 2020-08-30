f09cccf0ca4b07c4b946a4f23dc70a53048a3b94
Ori Dabush <dabushori@gmail.com>
2020-08-30 03:15:01 +0300

    changed mat_parser

--- src/mat_parser.cpp
+++ src/mat_parser.cpp
@@ -1,6 +1,7 @@
 #include "mat_parser.h"
 
 #include <algorithm>
+#include <iostream>
 
 namespace mat_parser {
 std::vector<std::string> matToText(const matrix::Mat &matrix) {
@@ -13,16 +14,35 @@ std::vector<std::string> matToText(const matrix::Mat &matrix) {
         line.append(",");
       }
     }
-    text[i] = line;
+    text.push_back(line);
   }
   return text;
 }
 
+void deleteSpaces(std::string &str) {
+  for (int i = 0; i < str.size(); ++i) {
+    if (str[i] == ' ' || str[i] == '\t') {
+      str.erase(i);
+    }
+  }
+}
+
 matrix::Mat textToMat(const std::vector<std::string> &text) {
-  std::for_each(text.begin(), text.end(), [](std::string &str) {
-    auto newEnd = std::remove_if(str.begin(), str.end(),
-                                 [](char c) { return c == ' ' || c == '\t'; });
-    str.erase(++newEnd, str.end());
-  });
+  std::vector<std::string> copy = text;
+  for (auto it = copy.begin(); it != copy.end(); ++it) {
+    deleteSpaces(*it);
+  }
+
+  uint32_t height = text.size(),
+           width = std::count_if(text[0].begin(), text[0].end(),
+                                 [](const char &c) { return c == ','; }) +
+                   1;
+
+  matrix::Mat matrix(height, width);
+  for (uint32_t i = 0; i < height; ++i) {
+    for (uint32_t j = 0; j < width; ++j) {
+    }
+  }
+  return matrix;
 }
 } // namespace mat_parser
